7379eaae6e6759ce54792af71907f3ddba535efe
HS980924 <75660071+HS98094@users.noreply.github.com>
2023-12-15 17:58:44 +0900

    fix signup router

--- BE/src/routes/authRouter.ts
+++ BE/src/routes/authRouter.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+import { signUpvalidationCheck } from '../middlewears/signupValidator';
 import authController from '../controllers/authController';
 
 const router: Router = Router();
 
-router.post('/signup',authController.signUp);
-router.post('/login',authController.login);
+router.post('/signup',[
+    body('email').isEmail(),
+    body('password').isStrongPassword({minLength: 3, minUppercase:0, minLowercase:1, minNumbers:1, minSymbols:1}),
+    body('username').isLength({min:1}),
+    signUpvalidationCheck
+    ], authController.signUp);
+
+router.post('/login',[
+    body('email').isEmail(),
+    body('password').isStrongPassword({minLength: 3, minUppercase:0, minLowercase:1, minNumbers:1, minSymbols:1}),
+    signUpvalidationCheck
+    ],authController.login);
 
 
 export default router;
