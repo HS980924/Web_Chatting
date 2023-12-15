6759c5fd3f88d9b9e7eda20dbe64b756930c2b7e
HS980924 <75660071+HS98094@users.noreply.github.com>
2023-12-15 17:59:53 +0900

    add authController about signup

--- BE/src/controllers/authController.ts
+++ BE/src/controllers/authController.ts
@@ -1,10 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
+import { signUpUser } from '../services/authService';
 
-const signUp = async(req: Request, res: Response, next:NextFunction) => {
+const signUp = async(req: Request, res: Response) => {
+    try{
+        const createUser = await signUpUser(req.body);
 
-    const { email, password } = req.body;
+        if(createUser){
+            return res.status(200).json({message: "회원가입이 완료되었습니다."});
+        }
+
+        return res.status(401).json({message: "이미 등록된 이메일 입니다."});
+    }catch(e){
+        res.status(500).json(e);
+    }
     
-
 }
 
 const login = async(req: Request, res: Response, next:NextFunction)  => {
