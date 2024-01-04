ef1a4e6dff8b62b01a40e7b145635c6ca015d453
HS980924 <75660071+HS98094@users.noreply.github.com>
2024-01-04 22:56:42 +0900

    feature: message socket

--- BE/src/socket/index.ts
+++ BE/src/socket/index.ts
@@ -1,20 +1,58 @@
+import express from 'express'
 import http from "http";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
+import Chatting from '../models/Chatting';
+import Room from '../models/Room';
+import Participant from '../models/Participant';
 
-export const Websocket = ( server:http.Server, app: Express.Application ) => {
+export const Websocket = ( server:http.Server, app: express.Application ) => {
     const io = new Server(server, { path: '/webchat' });
+    app.set('io',io);
+
+    // const room = io.of('room');
+    // const chat = io.of('chat');
 
     io.on("connection", (socket) => {
         const req = socket.request;
-        const ip = req.headers['x-forwarded-for'];
+        const ip:string | string[] | undefined = req.headers['x-forwarded-for'];
         console.log("New client connection!", ip, socket.id);
-        socket.on('disconnect',()=>{
-            console.log("Client disconnection", ip, socket.id);
-            // clearInterval(socket.interval);
-        });
-        socket.on('error', (error)=>{
-           console.error(error); 
-        });
-
+        disconnect(socket, ip);
+        creaet_message(socket, io);
+        error(socket);
     });
 };
+
+const disconnect = (socket: Socket, ip:string | string[] | undefined) => {
+    socket.on('disconnect',()=>{
+        console.log("Client disconnection", ip, socket.id);
+    });
+};
+
+const error = (socket: Socket) => {
+    socket.on('error', (error)=>{
+        console.error(error);
+    });
+}
+
+const creaet_message = (socket: Socket, io: Server) => {
+    socket.on('message', async (req) => {
+        const { room_id, user_id, message, not_read_userCnt } = req;
+        const newMessage = await Chatting.create({
+            room_id,
+            user_id,
+            message,
+            not_read_userCnt,
+        });
+
+        await Room.update(
+            { last_message: message },
+            { where: { room_id }}
+        );
+
+        const roomId = (room_id).toString()
+        
+        io.to(roomId).emit('message',newMessage);
+        
+    });
+};
+
